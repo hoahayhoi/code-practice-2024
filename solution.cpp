04a4de8f460b8e53c82a1e0e2010295c23fc3947
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-17 16:43:12 +0700

    Best time to buy and sell stock

--- solution.cpp
+++ solution.cpp
@@ -2,31 +2,47 @@
 #include <iostream>
 using namespace std;
 
-
+// BruceForce O(n^2)
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-
-        while (l <= r) {
-            int m = l + ((r - l) / 2); // m = (l + r) / 2 if not consider overflow
-            if (nums[m] < target) 
-                l = m + 1;
-            else if (nums[m] > target)
-                r = m - 1;
-            else 
-                return m;
+    int maxProfit(vector<int>& prices) {
+        int res = 0; 
+        for(int i = 0; i < prices.size() - 1; i++) {
+            for(int j = i + 1; j < prices.size(); j++) {
+                int diff = prices[j] - prices[i];
+                if (prices[j] > prices[i] && diff > res) {
+                    res = diff;
+                }
+            }
         }
-        return -1;
+        return res;
+    }
+};
+
+// Two poiter Mem: O(1), T : O(n)
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1; 
+        int res = 0; 
+        while (r < prices.size()) {
+            int diff = prices[r] - prices[l];
+            if (prices[r] > prices[l]) {
+                res = max(diff, res);
+            }
+            else {
+                l = r; 
+            }
+            r++;
+        }
+        return res;
     }
 };
 
 int main()
 {
-    Solution solution;
-    vector <int> v = {-1,0,3,5,9,12};
-    int target = 2; 
-    int rs = solution.search(v, target);
-    cout << "result " << rs; 
+    Solution2 solution;
+    vector <int> v = {7,1,5,3,6,4};
+    cout << "Max profit: " << solution.maxProfit(v);
     return 0;
 }
